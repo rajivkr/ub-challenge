a9cf093b37867903e87e0d75270e4eda94256579
Rajiv K.R <rajiv270992@gmail.com>
2020-07-10 13:20:17 +0530

    adding babel node

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ const findMissingNumber = (nums = []) => {
   return 0;
 };
 
-console.log(findMissingNumber([3, 2, 3]));
+if (process.env.NODE_ENV !== 'test') {
+  console.log(findMissingNumber([3, 2, 3]));
+}
 
-module.exports = findMissingNumber;
+export default findMissingNumber;
