589a0c820f1ed8cbef6373340056ae8fbbd48232
Rajiv K.R <rajiv270992@gmail.com>
2020-07-10 13:01:20 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const findMissingNumber = (nums = []) => {
+  if (nums.length === 1) return nums[0];
+  const countObj = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (countObj[nums[i]]) {
+      countObj[nums[i]] += 1;
+      if (countObj[nums[i]] > nums.length / 2) {
+        return nums[i];
+      }
+    } else {
+      countObj[nums[i]] = 1;
+    }
+  }
+  return 0;
+};
+
+console.log(findMissingNumber([3, 2, 3]));
+
+module.exports = findMissingNumber;
